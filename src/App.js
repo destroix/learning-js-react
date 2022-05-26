febf58426cb75bf4caa71bdb8efbba34ab127c53
destroix <evgeny.yuntsevich@gmail.com>
2022-05-27 01:21:22 +0300

    Hello React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Content from "./components/Content/Content";
 
-function App() {
+
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Hello React
-                </a>
-            </header>
+        <div className='app_wrapper'>
+            <Header/>
+            <Navbar/>
+            <Content/>
+            <Footer/>
         </div>
-    );
+    )
+
 }
 
 export default App;
